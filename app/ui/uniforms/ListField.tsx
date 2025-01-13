cf8bbd1331fde748d37c7088d0c7808957b7e3db
Yoaquim Cintrón <yoaquim@outlook.com>
2025-01-13 14:55:43 -0400

    Fix bug in uniforms component

--- app/ui/uniforms/ListField.tsx
+++ app/ui/uniforms/ListField.tsx
@@ -1,49 +1,48 @@
 import React, {
-  Children,
-  cloneElement,
-  isValidElement,
-  ReactElement,
-} from 'react';
-import { HTMLFieldProps, connectField, filterDOMProps } from 'uniforms';
+    Children,
+    cloneElement,
+    isValidElement,
+    ReactElement,
+} from 'react'
+import { HTMLFieldProps, connectField, filterDOMProps } from 'uniforms'
 
-import ListAddField from './ListAddField';
-import ListItemField from './ListItemField';
+import ListAddField from './ListAddField'
+import ListItemField from './ListItemField'
 
 export type ListFieldProps = HTMLFieldProps<
-  unknown[],
-  HTMLUListElement,
-  { itemProps?: object }
+    unknown[],
+    HTMLUListElement,
+    { itemProps?: object }
 >;
 
 function List({
-  children = <ListItemField name="$" />,
-  itemProps,
-  label,
-  value,
-  ...props
-}: ListFieldProps) {
-  return (
-    <ul {...filterDOMProps(props)}>
-      {label && (
-        <label>
-          {label}
-          <ListAddField name="$" />
-        </label>
-      )}
+                  children = <ListItemField name="$"/>,
+                  itemProps,
+                  label,
+                  value,
+                  ...props
+              }: ListFieldProps) {
+    return (
+        <ul {...filterDOMProps(props)}>
+            {label && (
+                <label>
+                    {label}
+                    <ListAddField name="$"/>
+                </label>
+            )}
 
-      {value?.map((item, itemIndex) =>
-        Children.map(children, (child, childIndex) =>
-          isValidElement(child)
-            ? cloneElement(child as ReactElement, {
-                key: `${itemIndex}-${childIndex}`,
-                name: child.props.name?.replace('$', '' + itemIndex),
-                ...itemProps,
-              })
-            : child,
-        ),
-      )}
-    </ul>
-  );
+            {value?.map((itemIndex) =>
+                Children.map(children, (child, childIndex) =>
+                    isValidElement(child)
+                        ? cloneElement(child as ReactElement, {
+                            key: `${itemIndex}-${childIndex}`,
+                            ...itemProps,
+                        })
+                        : child,
+                ),
+            )}
+        </ul>
+    )
 }
 
-export default connectField<ListFieldProps>(List);
+export default connectField<ListFieldProps>(List)
