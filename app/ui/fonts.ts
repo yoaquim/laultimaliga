41426cace2fe2c1b68206e8b30f03b4bc9dc4450
Yoaquim Cintrón <yoaquim@outlook.com>
2025-07-17 12:48:18 -0400

    General fixes

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Roboto, Jersey_10 } from 'next/font/google'
+import { Roboto, Jersey_10 } from "next/font/google";
 
-export const roboto = Roboto({subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900']})
-export const jersey10 = Jersey_10({subsets: ['latin'], weight: ['400']})
+export const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"] });
+export const jersey10 = Jersey_10({ subsets: ["latin"], weight: ["400"] });
